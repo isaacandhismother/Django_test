c1d5a8d33fede2deff4dcd59db1a7c90958f62a9
isaacandhismother <vitaliboshik40@gmail.com>
2024-04-11 21:49:28 +0400

    update

--- Website/market/admin.py
+++ Website/market/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('name', 'category', 'page_count', 'author_name', 'price')
     search_fields = ['name', 'category', 'author_name']
-
-
-admin.site.register(Book, BookAdmin)
