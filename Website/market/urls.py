337d600ee2eecdfd966893b8e8d924b1d9c851d8
isaacandhismother <vitaliboshik40@gmail.com>
2024-04-11 14:21:42 +0400

    update

--- Website/market/urls.py
+++ Website/market/urls.py
@@ -2,6 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.return_json),
-    path('home/', views.index)
+    path('', views.BookStore.as_view(), name='home')
 ]
--- Website/market/views.py
+++ Website/market/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.generic import ListView
+from django.core.paginator import Paginator
+
 from .models import Book
+
+
+class BookStore(ListView):
+    paginate_by = 4
+    model = Book
+    template_name = 'market/market_home.html'
+    context_object_name = 'book_store'
+    extra_context = {'title': 'Home page'}
 
 
 def index(request):
